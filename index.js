e9cd959bc4cd48cb8ddd8a1cbcf417b7837b1d99
gary909 <garywhite61@hotmail.com>
2020-06-16 17:46:40 +0200

    oops forgot to save

---
+++ index.js
@@ -0,0 +1,7 @@
+// A very simple kata from;
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+// Write a function "greet" that returns "hello world!"
+function greet() {
+    return 'hello world!';   // The function returns the product of p1 and p2
+  }
